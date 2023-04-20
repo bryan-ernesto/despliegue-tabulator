adc0f5baa54477f27a8eafa47623ea1252f696a9
Bryan Gamez <bgamez@groupnova.com.gt>
2023-04-20 12:12:59 -0600

    Proyecto, despliegue de información con Tabulator

---
+++ public/main.js
@@ -0,0 +1,23 @@
+var table = new Tabulator("#example-table", {
+  layout: "fitColumns",
+  columns: [],
+  pagination: "local",
+  paginationSize: 25,
+  paginationSizeSelector: [10, 25, 50, 100],
+  ajaxURL: "http://192.168.0.8:3000/api/recepciones_documento/Get_Prueba",
+  ajaxResponse: function (url, params, response) {
+    var columns = [];
+    var headers = Object.keys(response[0]);
+    headers.forEach((header) => {
+      columns.push({ title: header, field: header, headerFilter: "input" });
+    });
+    table.setColumns(columns);
+    return response;
+  },
+});
+
+table.setData();
+
+function exportTable() {
+  table.download("csv", "tabla.csv");
+}
